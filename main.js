48c8ed34e5bb7321d2815586d52be45e10c6f6b7
mizzyjo <86835729+mizzyjo@users.noreply.github.com>
2022-04-01 22:41:59 +0900

    JS: addItem function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//input new items
+//input new items by Enter
 const input = document.querySelector(".footer__input");
 let inputValue = null;
 
@@ -8,24 +8,41 @@ const items = document.querySelector(".items");
 
 input.addEventListener("keypress", (event) => {
   if (event.key == "Enter") {
-    inputValue = input.value;
-    console.log(inputValue);
-    const item__row = document.createElement("li");
-    item__row.className = "item__row";
-    item__row.innerHTML = `
+    addItem(event);
+  }
+});
+
+// input new items by button
+const plusBtn = document.querySelector(".footer__button");
+plusBtn.addEventListener("click", (event) => {
+  addItem(event);
+});
+
+// remove items
+const deleteBtn = document.querySelector(".item__delete");
+deleteBtn.addEventListener("click", (event) => {
+  deleteBtn.parentElement.parentElement.remove();
+  console.log(event.currentTarget);
+});
+
+function addItem(event) {
+  inputValue = input.value;
+  console.log(inputValue);
+  const item__row = document.createElement("li");
+  item__row.className = "item__row";
+  item__row.innerHTML = `
       <div class="item">
         <span class="item__name">${inputValue}</span>
         <button class="item__delete">
           <i class="fas fa-trash-alt"></i>
         </button>
       </div>
-      <div class="itme__divider"></div>
+      <div class="item__divider"></div>
     `;
 
-    items.append(item__row);
-    console.log(event.currentTarget);
-    console.log(event.target);
-    // event.currentTarget.value = "";
-    event.target.value = "";
-  }
-});
+  items.append(item__row);
+  // console.log(event.currentTarget);
+  // console.log(event.target);
+  // event.currentTarget.value = "";
+  event.target.value = "";
+}
