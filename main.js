1888172c7b02e3f65dfecc9b68c80dda6376d745
mizzyjo <86835729+mizzyjo@users.noreply.github.com>
2022-04-07 15:38:41 +0900

    JS: deleteBtn event delegation

--- main.js
+++ main.js
@@ -1,65 +1,76 @@
-"use strict";
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
 
-//input new items by Enter
-const input = document.querySelector(".footer__input");
-let inputValue = null;
-
-const items = document.querySelector(".items");
-let cnt = 0;
-
-input.addEventListener("keypress", (event) => {
-  if (event.key == "Enter") {
-    addItem(event);
-    removeItem(cnt);
-  }
-});
-
-// input new items by button
-const plusBtn = document.querySelector(".footer__button");
-plusBtn.addEventListener("click", (event) => {
-  addItem(event);
-  removeItem(cnt);
-});
-
-function addItem(event) {
-  inputValue = input.value;
-  console.log(inputValue);
-  const item__row = document.createElement("li");
-  item__row.className = "item__row";
-  item__row.innerHTML = `
-      <div class="item" data-rowcnt=${cnt}>
-        <span class="item__name">${inputValue}</span>
-        <button class="item__delete" data-delcnt=${cnt}>
-          <i class="fas fa-trash-alt"></i>
-        </button>
-      </div>
-      <div class="item__divider"></div>
-    `;
-
-  cnt++;
-  items.append(item__row);
-  event.target.value = "";
-}
-
-// remove items
-let deleteBtn = null;
-let item = null;
-
-function removeItem(cnt) {
-  deleteBtn = document.querySelectorAll(".item__delete");
-  item = document.querySelectorAll(".item");
-  if (deleteBtn === null || item === null) {
+function onAdd() {
+  // 1. 사용자가 입력한 텍스트를 받아옴
+  const text = input.value;
+  console.log(text);
+  if (text === '') {
+    input.focus();
     return;
   }
-  for (let i = 0; i < deleteBtn.length; i++) {
-    deleteBtn[i].addEventListener("click", (event) => {
-      console.log("del btn is clicked");
-      console.log(item[i].dataset.rowcnt);
-      console.log(deleteBtn[i].dataset.delcnt);
-      if (item[i].dataset.rowcnt == deleteBtn[i].dataset.delcnt) {
-        deleteBtn[i].parentElement.parentElement.remove();
-        console.log(deleteBtn[i].parentElement.parentElement);
-      }
-    });
-  }
+
+  // 2. 받아온 텍스트로 새로운 아이템을 만듦( 받아온 텍스트 + 삭제 버튼 )
+  const item = createItem(text);
+
+  // 3. items 컨테이너 안에 새로 만든 아이템을 추가한다
+  items.appendChild(item);
+
+  // 4. 새로 추가된 아이템으로 스크롤링(이동)
+  item.scrollIntoView({ block: 'center' });
+
+  // 5. 인풋을 초기화 함
+  input.value = '';
+  input.focus();
 }
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  // deleteBtn.addEventListener('click', () => {
+  //   items.removeChild(itemRow);
+  // });
+
+  items.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+      // console.log(event.currentTarget);
+      console.log(event.target);
+      // items.removeChild(itemRow);
+      event.target.parentNode.remove();
+    } else if (event.target.tagName == 'I') {
+      event.target.parentNode.parentNode.parentNode.remove();
+    }
+  });
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
+});
